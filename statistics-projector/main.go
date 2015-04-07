3e46dc297b93e38e515319d9f2629d6c776d134f
Jonathan Fok kan <jfokkan@gmail.com>
2015-04-06 22:43:03 -0400

    ADDED example projection to influxdb

--- statistics-projector/main.go
+++ statistics-projector/main.go
@@ -2,12 +2,33 @@ package main
 
 import (
 	"fmt"
+	"github.com/influxdb/influxdb/client"
 	"github.com/jonfk/training-log-analysis/common"
 	"log"
+	"net/url"
 	"os"
+	"time"
 )
 
-type Statistics struct {
+const (
+	User         = "jonathan"
+	InfluxDBHost = "localhost"
+	InfluxDBPort = 8086
+	InfluxDB     = "traininglog"
+)
+
+var (
+	InfluxUser = "jonfk"
+	InfluxPwd  = "password"
+)
+
+func init() {
+	if os.Getenv("INFLUX_USER") == "" || os.Getenv("INFLUX_PWD") == "" {
+		log.Println("Env INFLUX_USER and INFLUX_PWD are unset, using default user and password")
+	} else {
+		InfluxUser = os.Getenv("INFLUX_USER")
+		InfluxPwd = os.Getenv("INFLUX_PWD")
+	}
 }
 
 func main() {
@@ -22,9 +43,119 @@ func main() {
 		log.Fatal("Error parsing yaml: %s\n", err)
 	}
 
-	calculateStatistics(traininglogs)
+	connection := initInfluxdB(InfluxDBHost, InfluxDBPort)
+
+	ProjectExercise(connection, "low bar squats", traininglogs)
 }
 
-func calculateStatistics(logs []common.TrainingLog) Statistics {
-	return Statistics{}
+func ProjectExercise(conn *client.Client, name string, logs []common.TrainingLog) {
+	var metricsToBeInserted []ExerciseMetric
+	for _, trainLog := range logs {
+		for _, exercise := range trainLog.Workout {
+			if exercise.Name == name {
+				pTime, err := time.Parse(common.Time_ref, trainLog.Date+" "+trainLog.Time)
+				if err != nil {
+					log.Printf("Error parsing time in %s\n", trainLog.Date)
+					continue
+				}
+
+				weight, err := common.ParseWeight(exercise.Weight)
+				if err != nil {
+					log.Printf("Error parsing weight for %s in %s with error %s\n", exercise.Name, trainLog.Date, err)
+					continue
+				}
+				// Intensity metric
+				metric := ExerciseMetric{
+					Name:      exercise.Name + "_intensity",
+					Username:  User,
+					Value:     weight.Value,
+					Unit:      weight.Unit,
+					Timestamp: pTime,
+				}
+				metricsToBeInserted = append(metricsToBeInserted, metric)
+			}
+		}
+	}
+	writePoints(conn, metricsToBeInserted)
+}
+
+func initInfluxdB(host string, port int) *client.Client {
+	u, err := url.Parse(fmt.Sprintf("http://%s:%d", host, port))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	conf := client.Config{
+		URL:      *u,
+		Username: InfluxUser,
+		Password: InfluxPwd,
+	}
+
+	con, err := client.NewClient(conf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dur, ver, err := con.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("Connection verified %v, %s", dur, ver)
+	return con
+}
+
+// queryDB convenience function to query the database
+func queryDB(con *client.Client, cmd string) (res []client.Result, err error) {
+	q := client.Query{
+		Command:  cmd,
+		Database: InfluxDB,
+	}
+	if response, err := con.Query(q); err == nil {
+		if response.Error() != nil {
+			return res, response.Error()
+		}
+		res = response.Results
+	}
+	return
+}
+
+type ExerciseMetric struct {
+	Name      string
+	Username  string
+	Unit      string
+	Value     float32
+	Timestamp time.Time
+}
+
+func writePoints(con *client.Client, metrics []ExerciseMetric) error {
+	var (
+		points = make([]client.Point, len(metrics))
+	)
+
+	for i := range metrics {
+		points[i] = client.Point{
+			Name: metrics[i].Name,
+			Tags: map[string]string{
+				"username": metrics[i].Username,
+				"unit":     metrics[i].Unit,
+			},
+			Fields: map[string]interface{}{
+				"value": metrics[i].Value,
+			},
+			Timestamp: metrics[i].Timestamp,
+			Precision: "s",
+		}
+	}
+
+	bps := client.BatchPoints{
+		Points:          points,
+		Database:        InfluxDB,
+		RetentionPolicy: "default",
+	}
+	_, err := con.Write(bps)
+	if err != nil {
+		return err
+	}
+	return nil
 }
