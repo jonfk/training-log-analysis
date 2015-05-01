bab0ac1ad3289a32b46bcda288243e8a2895826e
Jonathan Fok kan <jfokkan@gmail.com>
2015-04-30 22:49:45 -0400

    ADDED paused conventional deadlift to valid exercises

--- common/common.go
+++ common/common.go
@@ -69,6 +69,7 @@ var validExerciseNames = []string{
 	// Pulling
 	"sumo deadlift",
 	"conventional deadlift",
+	"paused conventional deadlift",
 	"stiff leg deadlift",
 	"deficit conventional deadlift",
 	"block pulls",
