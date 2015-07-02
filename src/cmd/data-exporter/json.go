731b8b7a388699a91a49e1b7757bc95dc5428331
Jonathan Fok kan <jfokkan@gmail.com>
2015-07-02 19:30:29 -0400

    Feature: Added squats, bench and deadlift projections to json exporter

--- src/cmd/data-exporter/json.go
+++ src/cmd/data-exporter/json.go
@@ -38,19 +38,117 @@ func ExportJson(c *cli.Context) {
 
 	// Project Bodyweights
 	bodyweights := projections.ProjectBodyWeight(traininglogs)
-	var bodyweightDataPoints []DataPoint
-	for _, bodyweight := range bodyweights {
-		bodyweightDataPoints = append(bodyweightDataPoints, DataPoint{
-			Date:  bodyweight.Timestamp.Format(common.SimpleTimeRef),
-			Value: bodyweight.Value,
-		})
+	bodyweightjson, err := projectionToJson(bodyweights)
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	bodyweightjson, err := json.Marshal(bodyweightDataPoints)
+	// Project training duration
+	trainingDurations := projections.ProjectTrainingDuration(traininglogs)
+	trainingDurationjson, err := projectionToJson(trainingDurations)
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	// Project Squats
+	beltedLowBarSquatsIntensity := projections.ProjectExerciseIntensity(traininglogs, "belted low bar squats")
+	beltedLowBarSquatsIntensityJson, err := projectionToJson(beltedLowBarSquatsIntensity)
+	if err != nil {
+		log.Fatal(err)
+	}
+	lowBarSquatsIntensity := projections.ProjectExerciseIntensity(traininglogs, "low bar squats")
+	lowBarSquatsIntensityJson, err := projectionToJson(lowBarSquatsIntensity)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	squatsTonnage := projections.ProjectExerciseTonnage(traininglogs, "low bar squats", "high bar squats", "front squats", "belted low bar squats")
+	squatsTonnageJson, err := projectionToJson(squatsTonnage)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Project Bench
+	benchIntensity := projections.ProjectExerciseIntensity(traininglogs, "bench press", "close grip bench press")
+	benchIntensityJson, err := projectionToJson(benchIntensity)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	benchTonnage := projections.ProjectExerciseTonnage(traininglogs, "bench press", "close grip bench press")
+	benchTonnageJson, err := projectionToJson(benchTonnage)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Project Deadlift
+	deadliftIntensity := projections.ProjectExerciseIntensity(traininglogs, "sumo deadlift", "conventional deadlift")
+	deadliftIntensityJson, err := projectionToJson(deadliftIntensity)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	deadliftTonnage := projections.ProjectExerciseTonnage(traininglogs, "sumo deadlift", "conventional deadlift")
+	deadliftTonnageJson, err := projectionToJson(deadliftTonnage)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Write Json to file
 	err = ioutil.WriteFile(c.String("output")+string(os.PathSeparator)+"bodyweight.json", bodyweightjson, 493)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	err = ioutil.WriteFile(c.String("output")+string(os.PathSeparator)+"training_duration.json", trainingDurationjson, 493)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = ioutil.WriteFile(c.String("output")+string(os.PathSeparator)+"belted_low_bar_squats_intensity.json", beltedLowBarSquatsIntensityJson, 493)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = ioutil.WriteFile(c.String("output")+string(os.PathSeparator)+"low_bar_squats_intensity.json", lowBarSquatsIntensityJson, 493)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = ioutil.WriteFile(c.String("output")+string(os.PathSeparator)+"squats_tonnage.json", squatsTonnageJson, 493)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = ioutil.WriteFile(c.String("output")+string(os.PathSeparator)+"bench_intensity.json", benchIntensityJson, 493)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = ioutil.WriteFile(c.String("output")+string(os.PathSeparator)+"bench_tonnage.json", benchTonnageJson, 493)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = ioutil.WriteFile(c.String("output")+string(os.PathSeparator)+"deadlift_intensity.json", deadliftIntensityJson, 493)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = ioutil.WriteFile(c.String("output")+string(os.PathSeparator)+"deadlift_tonnage.json", deadliftTonnageJson, 493)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func projectionToJson(dataPoints []projections.DataPoint) ([]byte, error) {
+	var serializablePoints []DataPoint
+	for _, point := range dataPoints {
+		serializablePoints = append(serializablePoints, DataPoint{
+			Date:  point.Timestamp.Format(common.SimpleTimeRef),
+			Value: point.Value,
+		})
+	}
+	return json.Marshal(serializablePoints)
 }
